0ec5fd0239156a5303c6bc79b73c090025fa498e
satyam88288 <satyam828@gmail.com>
2022-08-22 13:36:09 +0530

    database change

--- database_connection.php
+++ database_connection.php
@@ -1,8 +1,8 @@
 <?php
 $server_name="localhost";
 $user_name="root";
-$password="akash@1234";
-$database_name="akash";
+$password="";
+$database_name="vision_login";
 
 $connect=mysqli_connect($server_name,$user_name,$password,$database_name) or die("Connection Fail");
 
