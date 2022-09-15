d14bc68745aedaa97a4f1bf3e0afc65e2e011a8d
Akash Singh <akashsngh681681@gmail.com>
2022-09-15 11:27:27 +0530

    db

--- database_connection.php
+++ database_connection.php
@@ -2,7 +2,7 @@
 $server_name="localhost";
 $user_name="root";
 $password="akash@1234";
-$database_name="akash";
+$database_name="vision_login";
 
 $connect=mysqli_connect($server_name,$user_name,$password,$database_name) or die("Connection Fail");
 
